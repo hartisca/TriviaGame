cb6b823b8bd05b699f770ee3b77828d59ada0cb9
Héctor <hartisca91@gmail.com>
2024-12-14 20:31:24 +0100

    Added handleClickOutside on dropdown menu

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import profileDefault from "../../assets/usuario.png";
 
 function Header() {
   const menus = ["Profile", "Records", "Logout"]
 
   const [ open, setOpen ] = useState(false)
+
+  const menuRef = useRef(null)
+  const imgRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if(menuRef.current && imgRef.current) {
+        if(
+          !menuRef.current.contains(e.target) &&
+          !imgRef.current.contains(e.target) 
+        ) {
+          setOpen(false)
+        }
+      }
+    }
+    document.addEventListener("click", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside)
+    }
+  }, []);
 
   return ( 
     <nav className="headerNav">
@@ -14,12 +35,16 @@ function Header() {
       <div className="profileContainer">
         <img src={profileDefault} alt="Profile picture" 
         className="profilePicture"
+        ref={ imgRef }
         onClick={ () => setOpen(!open)} />
        {open&&(
          <div className="dropDownMenu">
           <ul>
             {menus.map((menu) => (
-              <li key={menu}>{menu}</li>
+              <li key={menu}
+              ref={ menuRef }
+              onClick={ () => setOpen(false) }
+              >{menu}</li>
             ))}
           </ul>
         </div>
