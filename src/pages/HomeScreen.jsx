1c75e5b058d2e188f08c2cbe7366a6c9f088838d
Héctor <hartisca91@gmail.com>
2025-01-10 18:52:05 +0100

    Changed styles

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png"
 import Button from "../components/ui/Button";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function HomeScreen() {
 
@@ -14,7 +14,7 @@ function HomeScreen() {
       <section className="HomeSection">
         <img src={logo} alt="Quizzbro logo" width="550px" />
         <strong>Rules of the game: </strong><p className="rules">Every 3 correct answers, you will get a more difficult question from a random category. If you answer correctly, that category will be marked as completed. When you have completed all the categories, you win. Be careful you have only 30 questions!</p>
-        <p className="rules">Register if you want to get in the laderboard</p>
+        <p className="rules"><Link to="/register">Register</Link> if you want to get in the laderboard!</p>
         <Button text={"Start Playing"} onClick={handleStartPlaying} />
       </section>      
     </>    
